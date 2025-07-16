0e01686209d049cc488c4fd1f62bd09fded60176
test <test@test.fr>
2025-07-16 18:33:05 +0200

    update

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #OAR -n tir2vis
-#OAR -l /nodes=1/gpu=1,walltime=10:00:00
+#OAR -l /nodes=1/gpu=1,walltime=00:01:00
 #OAR -p gpumodel='V100'
 #OAR --stdout logs/print.out
 #OAR --stderr logs/error.err
