66f5c22ad6841717718d03433d217287f35b03c1
test <test@test.fr>
2025-07-16 15:55:31 +0200

    update

--- main.sh
+++ main.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #OAR -n tir2vis
-#OAR -l /nodes=1/gpu=1,walltime=01:00:00
-#OAR -p "gpumodel='A100'"
+#OAR -l /nodes=1/gpu=1,walltime=02:00:00
 #OAR --stdout logs/%jobid%.out
 #OAR --stderr logs/%jobid%.err
 #OAR --project pr-remote-sensing-1a
