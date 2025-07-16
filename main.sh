2dd7cdd35378be1777d3313577e383b1d50ec738
test <test@test.fr>
2025-07-16 15:27:53 +0200

    update

--- main.sh
+++ main.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-oarsub -n tir2vis
-oarsub -l nodes=2/gpu=6,walltime=01:00:00
-oarsub --stdout logs/%jobid%.out
-oarsub --stderr logs/%jobid%.err
-oarsub --projet pr-remote-sensing-1a
-
+#OAR -n tir2vis
+# Get a A100 GPU and all associated cpu and memory resources:
+#OAR -l /nodes=1/gpu=1 --project test -p "gpumodel='A100'" "nvidia-smi -L"
+#OAR --stdout logs/%jobid%.out
+#OAR --stderr logs/%jobid%.err
+#OAR --projet pr-remote-sensing-1a
 python ~/TIR2VIS/train.py
 
