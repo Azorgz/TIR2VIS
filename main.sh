c5f986c3c1e7897abe34a9e83f65b43015f6d3db
test <test@test.fr>
2025-07-16 16:48:48 +0200

    update

--- main.sh
+++ main.sh
@@ -2,8 +2,8 @@
 #OAR -n tir2vis
 #OAR -l /nodes=1/gpu=1,walltime=02:00:00
 #OAR -p gpumodel='V100'
-#OAR --stdout logs/%jobid%.out
-#OAR --stderr logs/%jobid%.err
+#OAR --stdout logs/print.out
+#OAR --stderr logs/error.err
 #OAR --project pr-remote-sensing-1a
 
 python ~/TIR2VIS/test.py
