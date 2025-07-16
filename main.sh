165bfd98168ab4ae2fe494a78ed2ff6d8cb321fa
test <test@test.fr>
2025-07-16 15:28:54 +0200

    update

--- main.sh
+++ main.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#OAR -n tir2vis
-# Get a A100 GPU and all associated cpu and memory resources:
+OAR -n tir2vis
 #OAR -l /nodes=1/gpu=1 --project test -p "gpumodel='A100'" "nvidia-smi -L"
 #OAR --stdout logs/%jobid%.out
 #OAR --stderr logs/%jobid%.err
