d9e4fbea06f89959b72beb81eaf2204a0695c82d
test <test@test.fr>
2025-07-16 16:29:39 +0200

    update

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #OAR -n tir2vis
-#OAR -l /nodes=1/gpu=1/migdevice=1,walltime=02:00:00 -t devel
+#OAR -l /nodes=1/gpu=1,walltime=02:00:00
 #OAR -p gpumodel='V100'
 #OAR --stdout logs/%jobid%.out
 #OAR --stderr logs/%jobid%.err
