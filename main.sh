53df31726f844d731c6eba160712ae7e27b3fd7d
test <test@test.fr>
2025-07-16 15:47:35 +0200

    update

--- main.sh
+++ main.sh
@@ -3,6 +3,5 @@
 #OAR -l /nodes=1/gpu=1 --project test -p "gpumodel='A100'" "nvidia-smi -L"
 #OAR --stdout logs/%jobid%.out
 #OAR --stderr logs/%jobid%.err
-#OAR --projet pr-remote-sensing-1a
+#OAR --project pr-remote-sensing-1a
 python ~/TIR2VIS/train.py
-
