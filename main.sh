87ed3e146c42421eda6ff7a3f38a1add78492866
test <test@test.fr>
2025-07-16 15:48:35 +0200

    update

--- main.sh
+++ main.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #OAR -n tir2vis
-#OAR -l /nodes=1/gpu=1 --project test -p "gpumodel='A100'" "nvidia-smi -L"
+#OAR -l /nodes=1/gpu=1 --project pr-remote-sensing-1a -p "gpumodel='A100'" "nvidia-smi -L"
 #OAR --stdout logs/%jobid%.out
 #OAR --stderr logs/%jobid%.err
-#OAR --project pr-remote-sensing-1a
 python ~/TIR2VIS/train.py
