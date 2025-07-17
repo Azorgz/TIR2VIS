81d23454a215cec23b88c6d6cd42de7692941aaf
test <test@test.fr>
2025-07-17 15:22:00 +0200

    update

--- main.sh
+++ main.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #OAR -n tir2vis
-#OAR -l /nodes=1/gpu=1,walltime=00:01:00
+#OAR -l /nodes=1/gpu=1,walltime=10:00:00
 #OAR -p gpumodel='V100'
 #OAR --stdout logs/print.out
 #OAR --stderr logs/error.err
 #OAR --project pr-remote-sensing-1a
 
-python ~/TIR2VIS/train.py --continue_train True --which_epoch 8 -- epoch_load 'latest'
+python ~/TIR2VIS/train.py --continue_train True --which_epoch 8 --epoch_load 'latest'
