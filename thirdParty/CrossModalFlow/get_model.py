da701c80c966769812b6bb649dbd3cadf11a88c2
test <test@test.fr>
2025-07-25 17:03:12 +0200

    update

--- thirdParty/CrossModalFlow/get_model.py
+++ thirdParty/CrossModalFlow/get_model.py
@@ -6,7 +6,7 @@ from .models.cross_raft import CrossRAFT
 
 def get_model():
     model = CrossRAFT(adapter=True)
-    path = os.getcwd() + '/thirdParty/CrossModalFlow/cross_raft_ckpt/model/checkpoint-10000.ckpt' if 'laptop' in socket.gethostname() else './bettik/PROJECTS/pr-remote-sensing-1a/godeta/checkpoints/FoalGAN_FLIR/flow/checkpoint-10000.ckpt'
+    path = os.getcwd() + '/thirdParty/CrossModalFlow/cross_raft_ckpt/model/checkpoint-10000.ckpt' if 'laptop' in socket.gethostname() else '/bettik/PROJECTS/pr-remote-sensing-1a/godeta/checkpoints/FoalGAN_FLIR/flow/checkpoint-10000.ckpt'
     state_dict = torch.load(path, weights_only=True)['state_dict']
     model.load_state_dict(state_dict)
     model = model.cuda().eval()
